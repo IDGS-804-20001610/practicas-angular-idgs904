8f0183e2eff1ea9e1286df89492fd972872be0aa
Brenda Ruiz <ruizhernandezbrendac@outlook.com>
2023-06-23 23:37:29 -0600

    BCRH - 2023/06/23 - RESISTENCIA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DistanceModule } from './distance/distance.module';
-import { ResistanceComponent } from './resistance/resistance.component';
 import { ResistanceModule } from './resistance/resistance.module';
-import { CinepolisComponent } from './cinepolis/cinepolis.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CinepolisComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
